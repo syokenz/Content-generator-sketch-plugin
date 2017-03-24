f876723fcb194c9f115d4f3ce014898abf06deb9
shoken <syokenz@gmail.com>
2017-03-24 12:48:06 +0900

    add ComboBox

--- CG.sketchplugin/Contents/Sketch/Images/KhPhotoApi.js
+++ CG.sketchplugin/Contents/Sketch/Images/KhPhotoApi.js
@@ -7,40 +7,60 @@ function onRun(context){
   var doc = context.document;
 
   if (selection.length > 0) {
-    var request = [[NSMutableURLRequest alloc] init];
-    [request setHTTPMethod:@"GET"];
-    var queryString = "https://kitchhike.com/api/photos_public";
-    [request setURL:[NSURL URLWithString:queryString]];
+    var alert = [NSAlert alertWithMessageText: "select target:"
+    defaultButton:"OK"
+    alternateButton:"Cancel"
+    otherButton:nil
+    informativeTextWithFormat:""];
 
-    var error = [[NSError alloc] init];
-    var responseCode = null;
+		var select = [[NSComboBox alloc] initWithFrame:NSMakeRect(0,0,200,25)];
+    var options = ["Pop-Up", "COOK", "HIKER", "Gallery"];
+    var query_options = ["popup", "cook", "hiker", "gallery"];
+		select.i18nObjectValues = options;
+		select.setEditable(false);
+		select.addItemsWithObjectValues(options);
+		select.selectItemAtIndex(0);
+    [alert setAccessoryView:select];
+    var button = [alert runModal];
 
-    var oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:responseCode error:error];
+    if (button == NSAlertDefaultReturn) {
+      var target = query_options[[select indexOfSelectedItem]];
+      var request = [[NSMutableURLRequest alloc] init];
+      [request setHTTPMethod:@"GET"];
+      var queryString = "https://kitchhike.com/api/photos_public?target=" + target;
+      [request setURL:[NSURL URLWithString:queryString]];
 
-    var dataString = [[NSString alloc] initWithData:oResponseData
-    encoding:NSUTF8StringEncoding];
+      var error = [[NSError alloc] init];
+      var responseCode = null;
 
-    var pattern = new RegExp("\\\\'", "g");
-    var validJSONString = dataString.replace(pattern, "'");
-    var data = JSON.parse(validJSONString);
+      var oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:responseCode error:error];
 
+      var dataString = [[NSString alloc] initWithData:oResponseData
+      encoding:NSUTF8StringEncoding];
 
-    if (data.items.length > 0) {
-      for (var i=0; i <= selection.length; i++) {
-        var randomIndex = Math.floor(Math.random()*(data.items.length-1));
-        var imageURLString = data.items[randomIndex].photo;
-        var url = [[NSURL alloc] initWithString: imageURLString];
-        var newImage = [[NSImage alloc] initByReferencingURL:url];
-        var layer = [selection objectAtIndex:i];
-        var fill = layer.style().fills().firstObject();
-        fill.setFillType(4);
-        fill.setImage(MSImageData.alloc().initWithImage_convertColorSpace(newImage, false));
-        fill.setPatternFillType(1);
+      var pattern = new RegExp("\\\\'", "g");
+      var validJSONString = dataString.replace(pattern, "'");
+      var data = JSON.parse(validJSONString);
+
+      if (data.items.length > 0) {
+        for (var i=0; i <= selection.length; i++) {
+          var randomIndex = Math.floor(Math.random()*(data.items.length-1));
+          var imageURLString = data.items[randomIndex].photo;
+          var url = [[NSURL alloc] initWithString: imageURLString];
+          var newImage = [[NSImage alloc] initByReferencingURL:url];
+          var layer = [selection objectAtIndex:i];
+          var fill = layer.style().fills().firstObject();
+          fill.setFillType(4);
+          fill.setImage(MSImageData.alloc().initWithImage_convertColorSpace(newImage, false));
+          fill.setPatternFillType(1);
+        }
+      } else {
+        var message = "No images found tagged with: ";
+        [doc showMessage: message];
       }
-    } else {
-      var message = "No images found tagged with: ";
-      [doc showMessage: message];
     }
+  } else {
+    [doc showMessage:"No layer is selected."];
   }
 }
 
